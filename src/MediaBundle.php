d225ec8b2820cc6a6ea8c642aa50544b45eab4d7
Arnaud BODEL <arnaud.bodel@gmail.com>
2024-07-25 01:19:43 +0200

    -

--- src/MediaBundle.php
+++ src/MediaBundle.php
@@ -1,14 +1,14 @@
 <?php 
 namespace OSW3\Media;
 
-use OSW3\Media\Utils\ConfigurationYaml;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use OSW3\Media\DependencyInjection\Configuration;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class MediaBundle extends Bundle
 {
     public function build(ContainerBuilder $container): void
     {
-        ConfigurationYaml::write($container->getParameter('kernel.project_dir'));
+        (new Configuration)->generateProjectConfig($container->getParameter('kernel.project_dir'));
     }
 }

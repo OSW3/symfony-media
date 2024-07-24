e50fff6946f71d60a0c26de677c1481d3feed0ce
Arnaud BODEL <arnaud.bodel@gmail.com>
2024-07-24 19:11:41 +0200

    -

--- src/MediaBundle.php
+++ src/MediaBundle.php
@@ -1,8 +1,8 @@
 <?php 
 namespace OSW3\Media;
 
+use OSW3\Media\Utils\ConfigurationYaml;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use OSW3\Media\DependencyInjection\ConfigurationYaml;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class MediaBundle extends Bundle

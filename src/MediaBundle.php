9ddeb797547680e842ba66c7fe4fc7a097ca57c0
Arnaud BODEL <53226044+ArnaudBDL@users.noreply.github.com>
2025-03-18 18:39:22 +0100

    Commit without message

--- src/MediaBundle.php
+++ src/MediaBundle.php
@@ -1,6 +1,8 @@
 <?php 
 namespace OSW3\Media;
 
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Filesystem\Path;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use OSW3\Media\DependencyInjection\Configuration;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -10,6 +12,28 @@ class MediaBundle extends Bundle
     public function build(ContainerBuilder $container): void
     {
         $projectDir = $container->getParameter('kernel.project_dir');
+
+
+        // Update dependency config
+        // -- 
+
+        $twig_component_Filepath = Path::join($projectDir, 'config/packages/twig_component.yaml' );
+        
+        if (file_exists($twig_component_Filepath))
+        {
+            $twig_component_YamlContent = file_get_contents( $twig_component_Filepath );
+            $twig_component_ArrayContent = Yaml::parse($twig_component_YamlContent);
+            
+            // Search Bundle Components namespace
+            $classPath = "OSW3\\Media\\Components\\";
+            $newClassPath = $classPath;
+
+            if (!isset( $twig_component_ArrayContent['twig_component']['defaults'][$newClassPath] ))
+            {
+                file_put_contents($twig_component_Filepath, Yaml::dump(array_merge_recursive($twig_component_ArrayContent, ['twig_component' => ['defaults' => [$newClassPath => "@Media/"]]]), 4));
+            }
+        }
+
 
 
         // Generate the YAML bundle configuration file in the project
